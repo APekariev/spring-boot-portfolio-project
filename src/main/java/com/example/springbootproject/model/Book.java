019306ab26304bb8e38c21fbc3e1de0b272630d0
Anton Pekariev <litsss30071989@gmail.com>
2026-08-09 18:59:55 +0300

    Mentor's comments are attended to (revision No.2)

--- src/main/java/com/example/springbootproject/model/Book.java
+++ src/main/java/com/example/springbootproject/model/Book.java
@@ -9,12 +9,10 @@ import jakarta.persistence.Table;
 import java.math.BigDecimal;
 import lombok.Getter;
 import lombok.Setter;
-import lombok.ToString;
 
 @Entity
 @Getter
 @Setter
-@ToString
 @Table(name = "books")
 public class Book {
 
@@ -36,6 +34,16 @@ public class Book {
     private String description;
     private String coverImage;
 
-    public Book() {
+    @Override
+    public String toString() {
+        return "Book{"
+                + "id=" + id
+                + ", title='" + title + '\''
+                + ", author='" + author + '\''
+                + ", isbn='" + isbn + '\''
+                + ", price=" + price
+                + ", description='" + description + '\''
+                + ", coverImage='" + coverImage + '\''
+                + '}';
     }
 }
